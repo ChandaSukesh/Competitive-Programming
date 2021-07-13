e841752295808aed5329d0f716578527ef3a86f9
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-13 17:00:27 +0530

    Version 1

--- 07-set_kth_digit-Java/MyMath.java
+++ 07-set_kth_digit-Java/MyMath.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  * Write the function setKthDigit(n, k, d) that takes three integers -- n, k, 
@@ -21,6 +23,84 @@ public class MyMath {
 	 */
 	public int setKthDigit(int n, int k, int d) {
 		// Your code goes here...
-		return 0;
+		boolean flag=false;
+		if(n<0)
+		{
+			n=-n;
+			flag=true;
+		}
+		int check=0;
+		int ans=0;
+		while(n!=0)
+		{
+			int last=n%10;
+			// System.out.println(n);
+			int power=(int)Math.pow(10,check);
+			
+			if(check==k)
+			{
+				ans=ans+(d *power) ;	
+			}
+			else
+			{
+				ans=ans+(last *power);
+			}
+			n=n/10;
+			check++;
+		}
+		
+		if(check==k)
+		{
+			int power=(int)Math.pow(10,k);
+		// System.out.println(power);
+			ans=ans+(d *power) ;
+		}
+		if(flag)
+		{
+			return -ans;
+		}
+		return ans;
+		// String nval=String.valueOf(n);
+		// if(k>nval.length())
+		// {
+			
+		// 	return n;
+		// }
+		
+
+		// // int[] arr=new int[nval.length()];
+		// ArrayList<Integer> arr=new ArrayList<>();
+		
+		// for(int i=0;i<nval.length();i++)
+		// {
+		// 	arr.add(nval.charAt(i) - '0');
+		// }
+
+		// int count=0;
+		// if(k==nval.length())
+		// {
+		// 	arr.set(k,d);
+		// 	count=1000;
+		// }
+		// else{
+		// 	k=nval.length()-k-1;
+		// 	// arr[k]=d;
+		// 	arr.set(k,d);
+		// 	count=100;
+		// }
+		
+	
+		// int res=0;
+		// int z=0;
+		// while(count!=0)
+		
+		// // for(int i=0;i<nval.length();i++)
+		// {
+		// 	res=res+ count*arr.get(z);
+		// 	count=count/10;
+		// 	z++;
+		// }
+
+		// return res;
 	}
 }
