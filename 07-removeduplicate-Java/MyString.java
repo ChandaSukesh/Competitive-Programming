ab30a3dbfb2b9dfbc3cc419cce2ea1c524db0b08
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-13 11:34:16 +0530

    Version 1

--- 07-removeduplicate-Java/MyString.java
+++ 07-removeduplicate-Java/MyString.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 // removeDuplicate(text) [10 pts]
 // Write a method removeDuplicate(text) that remove all the duplicate characters from a given input String
 // e.g. if given String is "JavaPython" then the output should be "JavPython".
@@ -6,6 +9,19 @@
 public class MyString {
 	public String removeDuplicate(String text) {
 		// Your code goes here
-		return "";
+		String res="";
+		Set<Character> hset=new HashSet<>();
+		
+		for(int i=0;i<text.length();i++)
+		{
+			if(!hset.contains(text.charAt(i)))
+			{
+				hset.add(text.charAt(i));
+				
+				res+=text.charAt(i);
+			}
+		}
+		
+		return res;
 	}
 }
