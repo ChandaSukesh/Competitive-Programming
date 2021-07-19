72520400fef429ad355df2f646964f5acfc4ca18
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-19 23:34:45 +0530

    Version 1

--- 03-applycaesarcipher-Java/CeaserCipher.java
+++ 03-applycaesarcipher-Java/CeaserCipher.java
@@ -12,7 +12,48 @@
 public class CeaserCipher {
 	public String fun_applycaesarcipher(String msg, int shift){
 		// your code goes here
-		return "";
+		int len=msg.length();
+		
+		String strvalue= "";
+		
+		for (int i=0;i<len;i++)
+		{
+			
+			if (msg.charAt(i)>='a' && msg.charAt(i)<='z'){
+				char ch=(char)(msg.charAt(i)+shift);
+				if(ch<97)
+				{
+					int val='z'-('a'-ch-1);
+					ch = (char)(val);
+				}
+				if(ch> 122)
+				{
+					int val='a'-'z' - 1;
+					ch = (char)(ch + val);
+				}
+				strvalue=strvalue+ch;
+			}
+			else if(msg.charAt(i)>='A' && msg.charAt(i)<='Z'){
+				char ch=(char)(msg.charAt(i)+shift);
+				if(ch<65)
+				{
+					int val='Z'-('A'-ch-1);
+					ch = (char)(val);
+				}
+				if(ch> 90)
+				{
+					int val1='A'-'Z' - 1;
+					ch = (char)(ch + val1);
+				}
+				strvalue=strvalue+ch;
+			}
+			else
+			{
+				strvalue= strvalue +msg.charAt(i);
+			}
+		}
+		return strvalue;
+		
 	}
 	
 }
