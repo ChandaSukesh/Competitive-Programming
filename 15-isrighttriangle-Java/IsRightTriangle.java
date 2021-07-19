8dfd057083e60eed4584a8d5e7e1e82496a2ec0a
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-19 22:49:55 +0530

    Version 1

---
+++ 15-isrighttriangle-Java/IsRightTriangle.java
@@ -0,0 +1,29 @@
+// isrighttriangle(x1, y1, x2, y2, x3, y3)[5pts]
+// Write the function isrighttriangle(x1, y1, x2, y2, x3, y3) that takes 6 int or float values that
+// represent the vertices (x1,y1), (x2,y2), and (x3,y3) of a triangle, and returns True if that is
+// a right triangle and False otherwise. You may wish to write a helper function,
+// distance(x1, y1, x2, y2), which you might call several times. Also, remember to use
+// almostEqual (instead of ==) when comparing floats.
+
+public class IsRightTriangle {
+	public boolean isRightTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
+		// Your code goes here
+		Integer side1=(int)(Math.pow((x2-x1),2)) + (int)Math.pow((y2-y1),2);
+		int side2=(int)(Math.pow((x3-x1),2)) + (int)Math.pow((y3-y1),2);
+		int side3=(int)(Math.pow((x1-x2),2)) + (int)Math.pow((y1-y2),2);
+
+		if( (side1.almostEqual(side2+side3) || side2==(side1+side3) || side3==(side1+side2)))
+		{
+			return true;
+		}
+		else{
+			
+				return false;
+		}
+		
+		
+		
+		
+		
+	}
+}
--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,13 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+
+	side1 = (int(pow((x2-x1),2)) + int(pow((y2-y1),2)))
+	side2 = (int(pow((x3-x1),2)) + int(pow((y3-y1),2)))
+	side3 = (int(pow((x3-x2),2)) + int(pow((y3-y2),2)))
+	if(side1==(side3+side2) or side2==(side1+side3) or side3==(side1+side2)):
+		return True
+	else:
+		return False
+		
+	
