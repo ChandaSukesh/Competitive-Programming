0efbb86283d0159cc15d02078b2fd0bacc7f45e7
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-20 09:17:07 +0530

    Version 1

--- 18-lineintersection-Java/LineIntersection.java
+++ 18-lineintersection-Java/LineIntersection.java
@@ -7,6 +7,42 @@
 public class LineIntersection {
 	public int lineIntersection(int m1, int b1, int m2, int b2) {
 		// Your code goes here
-		return 1;
+		if(b1==b2 || m1==m2)
+		{
+			return 0;
+		}
+		else if(checkFn(m1,m2))
+		{
+			return 0;
+		}
+		else if(checkFn(m2,m1))
+		{
+			return 0;
+		}
+		else
+		{
+			return (b2-b1)/(m1-m2);
+		}
+		
+	}
+
+	public boolean checkFn(int n1,int n2)
+	{
+		if (n1==0)
+		{
+			return true;
+		}
+		else if(n2==0)
+		{
+			return false;
+		}
+		else if (n1%n2==0){
+			return true;
+		}
+		
+		else
+		{
+			return false;
+		}
 	}
 }
