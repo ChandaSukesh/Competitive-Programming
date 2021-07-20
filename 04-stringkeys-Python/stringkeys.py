6a0b4608696ea60d15e3c1a8550893786f06f86c
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-20 12:06:02 +0530

    Version 1

--- 04-stringkeys-Python/stringkeys.py
+++ 04-stringkeys-Python/stringkeys.py
@@ -1,6 +1,6 @@
-"""Write a HashTable class that stores strings
-in a hash table, where keys are calculated
-using the first two letters of the string."""
+# """Write a HashTable class that stores strings
+# in a hash table, where keys are calculated
+# using the first two letters of the string."""
 
 class HashTable(object):
     def __init__(self):
@@ -11,19 +11,27 @@ class HashTable(object):
         the table."""
         # Hash Value = (ASCII Value of First Letter * 100) + ASCII Value of Second Letter 
         # Your code goes here
-        pass
+        self.table.insert(self.calculate_hash_value(string), string)
+        return -1
         
     def lookup(self, string):
         """Return the hash value if the
         string is already in the table.
         Return -1 otherwise."""
         # Your code goes here
-        pass
+        
+        if(self.table[self.calculate_hash_value(string)]==None):
+            return -1
+        elif(self.table[self.calculate_hash_value(string) ]!=string):
+            return -1
+        else:
+            return self.calculate_hash_value(string) 
 
     def calculate_hash_value(self, string):
         """Helper function to calulate a
         hash value from a string."""
         # Your code goes here
-        pass
-
-
+        firstChar=ord(string[0])*100
+        secondChar=ord(string[1])
+        hashValue=firstChar+secondChar
+        return hashValue
