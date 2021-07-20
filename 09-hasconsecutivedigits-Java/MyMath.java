0236685a7da7dc05122407b8710ef814c69d9f46
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-20 11:26:46 +0530

    Version 1

--- 09-hasconsecutivedigits-Java/MyMath.java
+++ 09-hasconsecutivedigits-Java/MyMath.java
@@ -5,6 +5,14 @@
 public class MyMath {
 	public boolean hasConsecutiveDigits(int n) {
 		// Your code goes here...
+		String strVal=String.valueOf(n);
+		for(int i=1;i<strVal.length();i++)
+		{
+			if(strVal.charAt(i)==strVal.charAt(i-1))
+			{
+				return true;
+			}
+		}
 		return false;
 	}
 }
