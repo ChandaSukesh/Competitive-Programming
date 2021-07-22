aa70bd532c565f6e435fb58884dc9e97dbacb9b1
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-22 11:33:08 +0530

    Version 1

--- 01-matrixmultiply-Java/matrixmultiply.java
+++ 01-matrixmultiply-Java/matrixmultiply.java
@@ -5,7 +5,24 @@
 
 class matrixmultiply {
 	public int[][] fun_matrixmultiply(int[][] a, int[][] b){
-		int[][] arr = {{}};
+		// System.out.println(a.length+" "+a[0].length+" "+b.length+" "+b[0].length+" ");
+		int[][] arr = new int[a.length][b[0].length];
+		
+		
+		
+			for(int i=0;i<a.length;i++)
+			{
+				for(int j=0;j<b[0].length;j++)
+				{
+					// arr[i][j]=0;
+					for(int k=0;k<b.length;k++)
+					{
+						arr[i][j]=arr[i][j] + (a[i][k]*b[k][j]);
+					}
+				}
+			}
+		
 		return arr;
+		
 	}
 }
