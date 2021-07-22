800a2f6acbc2cf1921b611a122a45b5503084c26
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-22 14:28:50 +0530

    Version 1

--- 06-lookandsay-Python/lookandsay.py
+++ 06-lookandsay-Python/lookandsay.py
@@ -8,7 +8,30 @@
 # lookAndSay([-1,2,7]) == [(1,-1),(1,2),(1,7)]
 # lookAndSay([3,3,8,-10,-10,-10]) == [(2,3),(1,8),(3,-10)]
 # lookAndSay([3,3,8,3,3,3,3]) == [(2,3),(1,8),(4,3)]
-
+import sys
 def lookandsay(a):
 	# Your code goes here
-	pass
+	lst = []
+	ans = 1
+	item = -sys.maxsize-1
+	
+	if a == lst:
+		return lst
+	else:
+		firstVal=a[0]
+		for i in a:
+			if i != item:
+				if item != (-sys.maxsize-1):
+					lst.append((ans, item))
+					item = i
+					ans = 1
+				else:
+					item = i
+					ans = 1
+			else:
+				item = i
+				ans += 1
+		lst.append((ans, item))
+	return lst
+
+print(lookandsay([[-1, 2, 7]]))
