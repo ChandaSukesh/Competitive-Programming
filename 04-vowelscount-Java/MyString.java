26540bb39cc46e0e7f7c0148788891dbf64c405c
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-23 09:11:10 +0530

    Version 1

--- 04-vowelscount-Java/MyString.java
+++ 04-vowelscount-Java/MyString.java
@@ -7,7 +7,17 @@
 public class MyString {
 	public int vowelCount(String s){
 		// Your code goes here...
-		return 0;
+		int count=0;
+		for(int i=0;i<s.length();i++)
+		{
+		
+			char ch=s.charAt(i);
+			if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+			{
+				count++;
+			}
+		}
+		return count;
 	}
 	
 }
