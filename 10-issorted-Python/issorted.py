de420ea66a065a0bffff46383081e5ed78e670d5
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-24 12:48:53 +0530

    Version 1

--- 10-issorted-Python/issorted.py
+++ 10-issorted-Python/issorted.py
@@ -7,4 +7,25 @@
 
 def issorted(a):
 	# your code goes here
-	pass
+	if(len(a)==0 or len(a)==1):
+		return True
+	asc=0
+	
+	if(a[1]>a[0]):
+		asc=1
+	else:
+		asc=0
+	
+	
+	for i in range(1,len(a)):
+		print(i)
+		if(asc==1):
+			if(a[i]<a[i-1]):
+				return False
+		else:
+			if(a[i]>a[i-1]):
+				return False
+	# for i in range(0,len(a)): 
+	# 	if(a[i]>a[i-1]):
+	# 		return False
+	return True
