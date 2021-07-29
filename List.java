6e710c6211648923e3c32fea0772df42ef91b7cd
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-29 10:56:59 +0530

    Version 1(Java)

--- List.java
+++ List.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 // nondestructiveRemoveRepeats(L)
 // Write the function nondestructiveRemoveRepeats(L), which takes a list L and nondestructively returns a new list in which any repeating elements in L are removed.
 // Specify the time complexity based on the solution that you have given.
@@ -10,8 +13,41 @@
 
 
 public class List {
-    public int[] nonDestructiveRemoveRepeats(int[] arr) {
+    public static void main(String args[])
+    {
         // Your code goes here....
-        return null;
+        int [] arr1={1, 3, 5, 3, 3, 2, 1, 7, 5};
+        int[] arr2={1, 3, 5, 2, 7};
+        
+        if(Arrays.equals(nondestructiveRemoveRepeats(arr1),arr2))
+        {
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+
+    public static int[] nondestructiveRemoveRepeats(int[] arr)
+    {
+        HashSet<Integer> hset=new HashSet<Integer>();
+        int count=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(!hset.contains(arr[i]))
+            {
+               arr[count]=arr[i];
+               count+=1;
+            }
+           hset.add(arr[i]);
+        }
+        
+        int ansarr[] =new int[count];
+        for(int i=0;i<count;i++)
+        {
+            ansarr[i]=arr[i];
+        }
+        return ansarr;
+        
     }
 }
