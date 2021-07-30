65ccf2927eaf8094063d725b76ad668d67a716f7
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-30 19:37:02 +0530

    Version 1

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,13 +10,40 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-        pass
+        if self.root==None:
+            self.root=Node(new_val)
+        else:
+            rootVal=self.root
+            upperVal=self.root
+
+            while(rootVal!=None):
+                upperVal=rootVal
+                if new_val<rootVal.value:
+                    rootVal=rootVal.left
+                else:
+                    rootVal=rootVal.right
+            if new_val<upperVal.value:
+                upperVal.left=Node(new_val)
+            else:
+                upperVal.right=Node(new_val)
+
+
+
+
 
     def printSelf(self):
         # Your code goes here
-        pass
+        print(self.root)
         
     def search(self, find_val):
         # Your code goes here
-        pass
+        while self.root!=None:
+            if self.root.value==find_val:
+                flag=True
+                return flag
+            elif self.root.value<find_val:
+                self.root=self.root.right
+            else:
+                self.root=self.root.left
+        return False
 
