b170687e6337452d0fefed3fb6357a96bdf9a1c7
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-07-30 10:36:38 +0530

    Version 1

--- Anagram.java
+++ Anagram.java
@@ -10,9 +10,60 @@
 // which could be quite handy here.
 // Hint: The time complexity can be achieved in Linear.
 
+import java.io.*;
+import java.util.*;
+
 public class Anagram {
     
     public static void main(String[] args) {
         // write your test cases here...
+        
+        String str1="act";
+        String str2="cat";
+        System.out.println(anagramFn(str1,str2));
+
+
     }
+    public static boolean anagramFn(String s1,String s2)
+    {
+        HashMap<Character,Integer> hmapA=new HashMap<>();
+        HashMap<Character,Integer> hmapB=new HashMap<>();
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+
+        if(s1.length()!=s2.length())
+        {
+            return false;
+        }
+        char arrs1[]=s1.toCharArray();
+        char arrs2[]=s2.toCharArray();
+        for(int i=0;i<s1.length();i++)
+        {
+            if(!hmapA.containsKey(arrs1[i]))
+            {
+                hmapA.put(arrs1[i],1);
+            }
+            else{
+                int count=hmapA.get(arrs1[i]);
+                hmapA.put(arrs1[i],count+1);
+            }
+        }
+        for(int i=0;i<s2.length();i++)
+        {
+            if(!hmapB.containsKey(arrs2[i]))
+            {
+                hmapB.put(arrs2[i],1);
+            }
+            else{
+                int count=hmapB.get(arrs2[i]);
+                hmapB.put(arrs2[i],count+1);
+            }
+        }
+        if(hmapA.equals(hmapB))
+        {
+            return true;
+        }
+        return false;
+    }
+
 }
