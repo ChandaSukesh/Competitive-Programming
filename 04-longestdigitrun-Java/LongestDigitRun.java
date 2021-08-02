407f057e4f0744f655e264de67f9fd65759f1316
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-02 13:29:48 +0530

    Version 1

--- 04-longestdigitrun-Java/LongestDigitRun.java
+++ 04-longestdigitrun-Java/LongestDigitRun.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 // longestDigitRun(n) [20 pts]
 // Write the function longestDigitRun(n) that takes a possibly-negative int value n and returns 
 // the digit that has the longest consecutive 
@@ -8,6 +12,41 @@
 public class LongestDigitRun {
 	public int longestDigitRun(int n) {
 		// Your code goes here
-		return -1;
+		HashMap<Integer,Integer> hmap=new HashMap<>();
+		n=Math.abs(n);
+		int prev=0;
+		while(n>0)
+		{
+			int m=n%10;
+			if(m!=prev)
+			{
+				hmap.put(m,1);
+			}
+			else{
+				if(!hmap.containsKey(m))
+				{
+					hmap.put(m,1);
+				}
+				else{
+					hmap.put(m,hmap.get(m) + 1);
+				}
+			}
+			
+			prev=m;
+			n=n/10;
+
+		}
+		int ans=Integer.MIN_VALUE;
+		int key=0;
+		for(Map.Entry e:hmap.entrySet())
+		{
+			int val=(int)e.getValue();
+			if(val>ans)
+			{
+				key=(int)e.getKey();
+				ans=val;
+			}
+		}
+		return key;
 	}
 }
