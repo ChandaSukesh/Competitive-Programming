c7141e43f03b2b9ac03d271625ef49b1c668c1f5
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-02 10:00:02 +0530

    Version 1

--- 07-samechars-Java/SameChars.java
+++ 07-samechars-Java/SameChars.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 // sameChars(s1, s2) [20 pts]
 // Write the function sameChars(s1, s2) that takes two strings and returns True if the two strings are composed of 
 // the same characters (though perhaps in different numbers and in different orders) -- that is, if every character 
@@ -8,6 +10,33 @@
 public class SameChars {
 	public boolean sameChars(String s1, String s2) {
 		// Your code goes here
+		HashSet<Character> a=new HashSet<Character>();
+		HashSet<Character> b=new HashSet<Character>();
+
+		char[] ch1=s1.toCharArray();
+		char[] ch2=s2.toCharArray();
+
+		for(int i=0;i<ch1.length;i++)
+		{
+			if(!a.contains(ch1[i]))
+			{
+				a.add(ch1[i]);
+			}
+		}
+
+		for(int i=0;i<ch2.length;i++)
+		{
+			if(!b.contains(ch2[i]))
+			{
+				b.add(ch2[i]);
+			}
+		}
+		if(a.equals(b))
+		{
+			return true;
+		}
+
+
 		return false;
 	}
 }
