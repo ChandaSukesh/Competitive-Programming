893cd51e2043680c35a2844e73a3222b685869b4
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-02 16:16:31 +0530

    Version 1

--- 08-nth_circularprime-Java/NthCircularPrime.java
+++ 08-nth_circularprime-Java/NthCircularPrime.java
@@ -8,6 +8,62 @@
 public class NthCircularPrime {
 	public long nthCircularPrime(int n) {
 		// your code goes here
-		return -1;
+		int count=0;
+		int found=0;
+		while(count<n)
+		{
+			found+=1;
+			if(checkPrime(found) && checkRotation(found))
+			{
+				count+=1;
+			}
+		}
+		return found;
+	}
+	public boolean checkPrime(int n)
+	{
+		if(n==2)
+		{
+			return true;
+		}
+		if(n<2 || n%2==0)
+		{
+			return false;
+		}
+		for(int i=3;i<=Math.sqrt(n);i=i+2)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean checkRotation(int n)
+	{
+		String str=String.valueOf(n);
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)=='0')
+			{
+				return false;
+			}
+		}
+		int len=str.length()-1;
+		int count=len;
+		int ans=0;
+		while(n>0)
+		{
+			int m=n%10;
+			ans =m*((int)Math.pow(10,count)) + ans;
+			count-=1;
+			n/=10;
+		}
+		if(checkPrime(ans))
+		{
+			return true;
+		}
+		return false;
 	}
 }
