36d27198c76c684016b7764decb50b52f77ac3c1
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-03 17:03:09 +0530

    Version 1

--- LatinSquare.java
+++ LatinSquare.java
@@ -1,3 +1,6 @@
+// import java.util.HashMap;
+import java.util.HashSet;
+
 // isLatinSquare(a)
 // Write the function isLatinSquare(a) that takes a 2d list 
 // and returns True if it is a Latin square and False otherwise.
@@ -8,9 +11,64 @@
 // Write your own test cases...
 
 public class LatinSquare {
-    public boolean isLatinSquare(int[][] arr) {
+    
+    public static void main(String[] args)
+    {
+        int[][] arr={{1,2,3},{3,1,2},{2,3,1}};
+        System.out.println(isLatinSquare(arr));
+    }
+    @SuppressWarnings("unchecked")
+    public static boolean isLatinSquare(int[][] arr) {
         // Your code goes here...
+        int len=arr.length;
+        HashSet<Integer>[] mapRow=new HashSet[len];
+        HashSet<Integer>[] mapCol=new HashSet[len];
+
+        for(int i=0;i<len;i++)
+        {
+            mapRow[i]=new HashSet<Integer>();
+            mapCol[i]=new HashSet<Integer>();
+        }
         
-        return false;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=0;j<len;j++)
+            {
+                mapRow[i].add(arr[i][j]);
+                mapCol[j].add(arr[i][j]);
+            }
+        }
+        int unUsedValue=0;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=0;j<len;j++)
+            {
+                if(arr[i][j] <=0 || arr[i][j]>len)
+                {
+                    unUsedValue+=1;
+                }
+            }
+        }
+
+        int rowVal=0;
+        int colVal=0;
+        for(int i=0;i<len;i++)
+        {
+            if(mapRow[i].size()!=len)
+            {
+                rowVal+=1;
+            }
+            if(mapCol[i].size()!=len)
+            {
+                colVal+=1;
+            }
+        }
+
+        if(unUsedValue !=0 || rowVal!=0 || colVal!=0)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
