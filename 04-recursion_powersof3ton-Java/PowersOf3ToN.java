909cd7a9652c4212434fa04d79cda0741594bd2c
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-05 10:03:01 +0530

    Version 1

--- 04-recursion_powersof3ton-Java/PowersOf3ToN.java
+++ 04-recursion_powersof3ton-Java/PowersOf3ToN.java
@@ -1,11 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Recursion-Only powersOf3ToN(n) [15 pts]
 // Write the function powersOf3ToN(n) that takes a possibly-negative float or int n, and returns a list of the 
 // positive powers of 3 up to and including n. As an example, powersOf3ToN(10.5) returns [1, 3, 9]. If no such powers 
 // of 3 exist, you should return the empty list. You may not use loops/iteration in this problem. 
 
 public class PowersOf3ToN {
-	public int[] powersOf3ToN(double n) {
+	public  int[] powersOf3ToN(double n) {
 		// Your code goes here
-		return new int[0];
+		int m=(int)Math.floor(n);
+		int count=0;
+      	ArrayList<Integer> arr=new ArrayList<>();
+		checkRecursion(m,count,arr);
+		
+		int[] ret = new int[arr.size()];
+		for (int i=0; i < ret.length; i++)
+		{
+			ret[i] = arr.get(i).intValue();
+		}
+		return ret;
 	}
+   
+	public  List<Integer> checkRecursion(int n,int power,List<Integer> arr)
+	{
+		int res=(int)Math.pow(3,power);
+		if(res>n)
+		{
+			return arr;
+		}
+      	arr.add(res);
+		// System.out.println("suk: "+arr.size());
+		return checkRecursion(n,power+1,arr);
+		
+	}
+	// public int[] powersOf3ToN(double n) {
+	// 	// Your code goes here
+	// 	int m=(int)Math.floor(n);
+	// 	int count=0;
+	// 	checkRecursion(m,count);
+	// 	return new int[0];
+	// }
+	// public List<Integer> checkRecursion(int n,int power)
+	// {
+	// 	ArrayList<Integer> arr=new ArrayList<>();
+	// 	if(power==0)
+	// 	{
+	// 		arr.add(1);
+	// 	}
+	// 	int res=(int)Math.pow(3,power);
+	// 	if(res>n)
+	// 	{
+	// 		return arr;
+	// 	}
+	// 	System.out.println("suk: "+arr.add(res));
+	// 	return checkRecursion(n,power+1);
+		
+	// }
 }
