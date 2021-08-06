e37a1630b6cdccea161ce7222d432ff7e0e4458e
Chanda Sukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-08-06 16:47:57 +0530

    Version 1

--- set.py
+++ set.py
@@ -7,6 +7,24 @@
 # 	limitedPowerSet(5, 7) => 
 # [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ]
 
+
+
 def limitedPowerSet(n, k):
     # Your code goes here...
-    pass
+    if(n<0):
+        return None
+    if(k<0):
+        return None
+    s = set(())
+    lst = list()
+    for i in range(1, n+1):
+        s.add(i)
+
+    for i in range(1, len(s)+1):
+        for j in range(len(lst)):
+                lst.append(l[j])
+            else:
+                return l
+
+assert(limitedPowerSet(5, 7) == [ {}, {1}, {2}, {3}, {4}, {5}, {1, 2} ])
+print("All test cases passed")
